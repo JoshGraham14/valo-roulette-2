2454f67847bf891eb267c2bc18f08747d2c12af5
JoshGraham <50626396+JoshGraham14@users.noreply.github.com>
2021-09-10 19:46:06 -0400

    Created list and detail views/urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from . import views
 
 urlpatterns = [
-
+    path('strats/', views.StratList.as_view(), name='strat-list'),
+    path('strats/<int:pk>/', views.StratDetail.as_view(), name='strat-detail')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Strat
+from .serializers import StratSerializer
+
+from rest_framework import generics
+
+
+class StratList(generics.ListCreateAPIView):
+    """List all strats"""
+    queryset = Strat.objects.all()
+    serializer_class = StratSerializer
+
+
+class StratDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Strat.objects.all()
+    serializer_class = StratSerializer
