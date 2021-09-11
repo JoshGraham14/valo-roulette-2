9686ffa9448899a3f7bfa0a672513dd1dd80998b
JoshGraham <50626396+JoshGraham14@users.noreply.github.com>
2021-09-11 14:01:55 -0400

    Added an AdminStrat class to improve the admin site

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from .models import Strat
 
-# Register your models here.
-admin.site.register(Strat)
+
+class StratAdmin(admin.ModelAdmin):
+    list_display = ('title', 'map_name', 'team')
+    list_filter = ('map_name', 'team')
+    search_fields = ['title', 'map_name', 'team']
+
+
+admin.site.register(Strat, StratAdmin)
